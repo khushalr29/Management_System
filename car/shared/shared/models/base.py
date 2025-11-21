90e4d8a35b2448885a4612f10bf80ea38e51376f
khushalrawal <114647728+khushalr29@users.noreply.github.com>
2025-11-22 00:04:42 +0530

    Delete car directory

--- car/shared/shared/models/base.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class BaseModel(models.Model):
-    created_at= models.DateTimeField(null=True , blank=True)
-    updated_at = models.DateTimeField(null=True , blank=True)
-    deleted_at= models.DateTimeField(null=True , blank=True)
-    
