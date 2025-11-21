3acde95c89277530911b1f4b75efd123df210c91
khushalrawal <114647728+khushalr29@users.noreply.github.com>
2025-11-21 23:50:36 +0530

    Delete car directory

--- car/brand_and_model_service/brands/serializers/brandSerializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from shared.shared.models.brand_and_model_Service.brand import Brand
-
-class BrandSerializer(serializers.ModelSerializer):
-    class Meta:
-        model= Brand
-        fields = "__all__"
--- car/brand_and_model_service/brands/serializers/varientsSerializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from shared.shared.models.brand_and_model_Service.varients import Varients
-
-class VarientsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model= Varients
-        fields = "__all__"
